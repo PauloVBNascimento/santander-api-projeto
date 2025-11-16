3e5e5dcef812db004f741d71044e7f97a49eafed
Paulo Nascimento <victinhopaulo64@gmail.com>
2025-11-16 10:05:33 -0300

    Primeiro Commit

---
+++ src/test/java/me/dio/santander_api_projeto/SantanderApiProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_api_projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderApiProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
